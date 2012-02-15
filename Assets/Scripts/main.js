ffe8e93faa9084446ed61d3bd471c6f3e92caca6
Mark McDonnell <markmcdonnell@Marks-MacBook-Pro.local>
2012-02-15 22:47:38 +0000

    Add subscriber form

--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -13,4 +13,24 @@ require(['errorhandler'], function(handler) {
 	require.onError = handler;
 });
 
+require(['jquery'], function($){
+    
+    $(function () {
+        $('#subForm').submit(function (e) {
+            e.preventDefault();
+            $.getJSON(
+            this.action + "?callback=?",
+            $(this).serialize(),
+            function (data) {
+                if (data.Status === 400) {
+                    alert("Error: " + data.Message);
+                } else { // 200
+                    alert("Success: " + data.Message);
+                }
+            });
+        });
+    });
+
+});
+
 require(['map']);
